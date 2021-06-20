9e8fdbf99765d9a4d7b0393734bd41c36f48272a
YongJun Lee <mlionlove54@gmail.com>
2021-06-20 21:45:49 +0900

    Securing the Keys
    - add env in securing the keys
    - firebase process env load
    
    referenced: #3

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDImFOGOih7yANKmzInK_QxNDeeXs434Ks",
-    authDomain: "nwitter-914bb.firebaseapp.com",
-    projectId: "nwitter-914bb",
-    storageBucket: "nwitter-914bb.appspot.com",
-    messagingSenderId: "1033615444579",
-    appId: "1:1033615444579:web:a167764b0bf5bf6f970656"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   export default firebase.initializeApp(firebaseConfig);
